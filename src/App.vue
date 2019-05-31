9716c492448c256a0959af9c63f0c5c1d5f5a972
steven waters <swaters86@gmail.com>
2019-05-31 13:20:03 -0400

    started developing film toolbox app after branching off from PWA template that is still needs to be fixed

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,73 @@
 <template>
-  <div>
-    <ul>
-      <li v-for="(link, index) in links" :key="index">
-        <router-link :to="link.to">{{ link.name }}</router-link>
-      </li>
-    </ul>
+  <div class="container">
+    <header class="app-header">
+      <h1 class="app-headline">Filmmaker's Toolbox</h1>
+      <p class="app-description">
+        A website that provides you handy links to the web's best resources when it comes to filmmaking. 
+        This website featuers a hand-currated list of apps , websites, gear, equipment, and other resources that are related to filmmaking.
+      </p>
+    </header>
     <main>
       <router-view/>
     </main>
+    <footer class="app-footer">
+      <p>
+        <small>Made With Love By Steven Waters © 2019</small>
+      </p>
+    </footer>
   </div>
 </template>
 
-<style scoped>
-  ul {
-    list-style: none;
-    display: flex;
-    padding: 0;
+<style>
+  *,
+  *:before,
+  *:after {
+    box-sizing: border-box;
   }
 
-  li {
-    padding: 5px 15px 5px 0;
+  body {
+    font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
+    background-color: #fff;
+    color: #444;
+    font-size: 16px;
+    
   }
 
-  li a {
-    text-decoration: none;
-    color: black;
+  .container {
+    width: 835px;
+    margin: 0 auto; 
   }
 
-  li a:hover {
-    color: #404040;
+  .app-header {
+    padding: 20px;
+    background: #333;
+    color: #efefef;
+    text-align: center; 
+  }
+
+  .app-header .app-headline {
+    font-size: 2rem;
+  }
+
+  .app-header .app-description {
+    font-size: 1.5rem;
+  }
+
+  @media screen and (max-width: 375px) {
+    .container {
+      width: 340px;
+    }
+
+    .app-header .app-headline {
+      font-size: 1.5rem;
+    }
+
+    .app-header .app-description {
+      font-size: 1rem;
+    }
   }
 </style>
 
 <script>
-export default {
-  data: () => ({
-    links: [
-      {
-        name: 'Home',
-        to: '/'
-      },
-      {
-        name: 'Page 1',
-        to: '/page-1'
-      },
-      {
-        name: 'Bad Link',
-        to: '/random-bad-url'
-      }
-    ]
-  })
-}
+
 </script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,100 @@
 <template>
-  <div>
-    <h1>Home</h1>
-  </div>
+  <section class="panels">
+    <div class="panel" v-for="(link, index) in links" :key="index">
+      <router-link :to="link.to">{{ link.name }}</router-link>
+    </div>
+  </section>
 </template>
 
 <style scoped>
-h1 {
-  color: blue
-}
+  .panels {
+    display: flex; 
+    flex-wrap: wrap;
+    justify-content: space-between; 
+  }
+
+  .panel { 
+    width: 200px;
+    height: 200px; 
+    margin-top: 10px;
+    padding: 20px;
+    background: #333;
+    line-height: 180px;
+    text-align: center; 
+  }
+
+  .panel a:link, .panel a:visited, .panel a:active {
+    color: #fff;
+    font-size: 1.5rem;
+  }
+
+
+  @media screen and (max-width: 375px) {
+    .panel {
+      width: 165px;
+      height: 165px; 
+      line-height: 145px;
+    }
+  }
 </style>
 
 <script>
+export default {
+  data: () => ({
+    links: [
+      {
+        name: 'Cameras',
+        to: '/cameras'
+      },
+      {
+        name: 'Gaffer\'s Tape',
+        to: '/gaffers-tape'
+      },
+      {
+        name: 'Bags',
+        to: '/bags'
+      },
+      {
+        name: 'Audio',
+        to: '/audio'
+      }, 
+      {
+        name: 'Smartphone Apps',
+        to: '/mobile-apps'
+      }, 
+      {
+        name: 'Screen Writing Software',
+        to: '/screen-writing-software'
+      }, 
+      {
+        name: 'Blogs',
+        to: '/blogs'
+      }, 
+      {
+        name: 'Misc. Websites',
+        to: '/misc-websites'
+      }, 
+      {
+        name: 'Sliders',
+        to: '/sliders'
+      }, 
+      {
+        name: 'Stabilizers',
+        to: '/stabilizers'
+      }, 
+      {
+        name: 'Lighting',
+        to: '/lighting'
+      },
+      {
+        name: 'Misc. Gear',
+        to: '/misc-gear'
+      },
+      {
+        name: 'Tutorials',
+        to: '/tutorials'
+      }
+    ]
+  })
+}
 </script>
